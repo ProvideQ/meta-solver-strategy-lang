3eb2fd8911f5f5d25552404ae21013a18aa0ec91
Elscrux <nickposer2102@gmail.com>
2025-05-20 20:06:08 +0200

    chore: Uncomment demo validation

--- src/language/meta-solver-strategy-validator.ts
+++ src/language/meta-solver-strategy-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { MetaSolverStrategyAstType, Person } from './generated/ast.js';
+import type { ValidationChecks } from 'langium';
+import type { MetaSolverStrategyAstType } from './generated/ast.js';
 import type { MetaSolverStrategyServices } from './meta-solver-strategy-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: MetaSolverStrategyServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.MetaSolverStrategyValidator;
     const checks: ValidationChecks<MetaSolverStrategyAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        // Person: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +19,13 @@ export function registerValidationChecks(services: MetaSolverStrategyServices) {
  */
 export class MetaSolverStrategyValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
+    // }
 
 }
