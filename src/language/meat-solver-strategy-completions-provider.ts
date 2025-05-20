5aea2f26c28059117ad41779314b8dc5b5887447
Elscrux <nickposer2102@gmail.com>
2025-05-20 21:34:16 +0200

    feat: Add completions for problem attributes, solvers, and keywords

--- src/language/meat-solver-strategy-completions-provider.ts
+++ src/language/meat-solver-strategy-completions-provider.ts
@@ -1,9 +1,10 @@
 import { CompletionAcceptor, CompletionContext, DefaultCompletionProvider, NextFeature } from "langium/lsp";
 import { CompletionItemKind } from "vscode-languageserver";
-import { ProblemType } from "./generated/ast.js";
+import { ProblemAttributeInt, ProblemType, SolverID, } from "./generated/ast.js";
+import { GrammarAST } from "langium";
 
 export class MetaSolverStrategyCompletionsProvider extends DefaultCompletionProvider {
-    protected override  async  completionFor(context: CompletionContext, next: NextFeature, acceptor: CompletionAcceptor): Promise<void> {
+    protected override  async completionFor(context: CompletionContext, next: NextFeature, acceptor: CompletionAcceptor): Promise<void> {
         console.log("TEST CompletionProvider", context, next, acceptor);
         if (next.type === ProblemType) {
             acceptor(context, {
@@ -18,8 +19,57 @@ export class MetaSolverStrategyCompletionsProvider extends DefaultCompletionProv
                 label: "QUBO",
                 kind: CompletionItemKind.EnumMember,
             })
+        } else if (next.type === SolverID) {
+            // When auto completing this it should automatically generate the subroutines calls too
+            acceptor(context, {
+                label: "Lkh3",
+                kind: CompletionItemKind.Function,
+            })
+            acceptor(context, {
+                label: "Cluster",
+                documentation: "", // TODO: Load description from solver
+                insertTextFormat: 2,
+                insertText: `Cluster(clusters=\${1:3}):
+\tSolve TSP[] \${2:tsps}:
+\t\t`,
+                kind: CompletionItemKind.Function,
+            })
+            acceptor(context, {
+                label: "Qrisp",
+                kind: CompletionItemKind.Function,
+            })
+        } else if (next.type === ProblemAttributeInt) {
+            acceptor(context, {
+                label: "size",
+                kind: CompletionItemKind.Property,
+            })
         } else {
             await super.completionFor(context, next, acceptor);
+        }
+    }
+
+    protected override async completionForKeyword(context: CompletionContext, keyword: GrammarAST.Keyword, acceptor: CompletionAcceptor): Promise<void> {
+        console.log("TEST CompletionProvider keyword", context, keyword, acceptor);
+        if (keyword.value === "if") {
+            acceptor(context, {
+                label: "if",
+                insertTextFormat: 2,
+                insertText: `if \${1:condition}:
+\t\${2}
+else:
+\t\${3}`,
+                kind: CompletionItemKind.EnumMember,
+            })
+        } else if (keyword.value === "foreach") {
+            acceptor(context, {
+                label: "foreach",
+                insertTextFormat: 2,
+                insertText: `foreach \${1:problem} in \${2:problems}:
+\t\${3}`,
+                kind: CompletionItemKind.EnumMember,
+            })
+        } else {
+            await super.completionForKeyword(context, keyword, acceptor);
         }
     }
 }
