ed2a66dfe4dd27e72d20abeab880c7cb6109ecfb
Elscrux <nickposer2102@gmail.com>
2025-06-17 12:00:50 +0200

    feat: add validators

--- src/language/meta-solver-strategy-validator.ts
+++ src/language/meta-solver-strategy-validator.ts
@@ -1,6 +1,8 @@
-import type { ValidationChecks } from 'langium';
-import type { MetaSolverStrategyAstType } from './generated/ast.js';
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import { MetaSolverStrategyAstType, ProblemType, ProblemTypes, SolverID } from './generated/ast.js';
 import type { MetaSolverStrategyServices } from './meta-solver-strategy-module.js';
+import * as api from "../api/ToolboxAPI.ts";
+import { getProblemTypeBySolverId } from './utils/ast-utils.ts';
 
 /**
  * Register custom validation checks.
@@ -9,7 +11,9 @@ export function registerValidationChecks(services: MetaSolverStrategyServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.MetaSolverStrategyValidator;
     const checks: ValidationChecks<MetaSolverStrategyAstType> = {
-        // Person: validator.checkPersonStartsWithCapital
+        ProblemType: validator.checkProblemTypeExists,
+        ProblemTypes: validator.checkProblemTypesExists,
+        SolverID: validator.checkSolverIDExists,
     };
     registry.register(checks, validator);
 }
@@ -19,13 +23,39 @@ export function registerValidationChecks(services: MetaSolverStrategyServices) {
  */
 export class MetaSolverStrategyValidator {
 
-    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-    //     if (person.name) {
-    //         const firstChar = person.name.substring(0, 1);
-    //         if (firstChar.toUpperCase() !== firstChar) {
-    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-    //         }
-    //     }
-    // }
+    async checkProblemTypeExists(problemType: ProblemType, accept: ValidationAcceptor): Promise<void> {
+        await api.initialize(); // Ensure problem types are initialized before checking
+        if (problemType.problemType) {
+            const type = await api.getProblemType(problemType.problemType);
+            if (type === undefined) {
+                accept('error', `Problem type '${problemType.problemType}' does not exist.`, { node: problemType, property: "problemType" }); 
+            }
+        }
+    }
+
+    async checkProblemTypesExists(problemTypes: ProblemTypes, accept: ValidationAcceptor): Promise<void> {
+        if (problemTypes.problemType) {
+            const type = await api.getProblemType(problemTypes.problemType.problemType);
+            if (type === undefined) {
+                accept('error', `Problem type '${problemTypes.problemType.problemType}' does not exist.`, { node: problemTypes, property: "problemType" }); 
+            }
+        }
+    }
+
+    async checkSolverIDExists(solverId: SolverID, accept: ValidationAcceptor): Promise<void> {
+        await api.initialize(); // Ensure problem types are initialized before checking
+        const problemTypeId = getProblemTypeBySolverId(solverId);
+        if (!problemTypeId) {
+            accept('error', `Solver ID '${solverId.solverId}' is not associated with any problem type.`, { node: solverId, property: "solverId" });
+            return;
+        }
+
+        if (solverId.solverId) {
+            const type = await api.getSolver(problemTypeId.id, solverId.solverId);
+            if (type === undefined) {
+                accept('error', `Solver '${solverId.solverId}' does not exist.`, { node: solverId, property: "solverId" }); 
+            }
+        }
+    }
 
 }
