85aa10d3ae2a90e3f5afe37ab67347fe6c8a43ae
Elscrux <nickposer2102@gmail.com>
2025-07-01 10:21:18 +0200

    feat: add solver setting name validation

--- src/language/meta-solver-strategy-validator.ts
+++ src/language/meta-solver-strategy-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import { MetaSolverStrategyAstType, ProblemType, ProblemTypes, SolverID } from './generated/ast.js';
+import { MetaSolverStrategyAstType, ProblemType, ProblemTypes, SolverID, SolverSetting } from './generated/ast.js';
 import type { MetaSolverStrategyServices } from './meta-solver-strategy-module.js';
 import * as api from "../api/ToolboxAPI.ts";
 import { getProblemTypeBySolverId } from './utils/ast-utils.ts';
@@ -14,6 +14,7 @@ export function registerValidationChecks(services: MetaSolverStrategyServices) {
         ProblemType: validator.checkProblemTypeExists,
         ProblemTypes: validator.checkProblemTypesExists,
         SolverID: validator.checkSolverIDExists,
+        SolverSetting: validator.checkSolverSettingExists,
     };
     registry.register(checks, validator);
 }
@@ -34,7 +35,6 @@ export class MetaSolverStrategyValidator {
     }
     
     async checkProblemTypesExists(problemTypes: ProblemTypes, accept: ValidationAcceptor): Promise<void> {
-        await api.initialize(); // Ensure problem types are initialized before checking
         if (problemTypes.problemType) {
             const type = api.getProblemType(problemTypes.problemType.problemType);
             if (type === undefined) {
@@ -52,9 +52,26 @@ export class MetaSolverStrategyValidator {
         }
 
         if (solverId.solverId) {
-            const type = await api.getSolver(problemTypeId.id, solverId.solverId);
-            if (type === undefined) {
+            const solver = await api.getSolver(problemTypeId.id, solverId.solverId);
+            if (solver === undefined) {
                 accept('error', `Solver '${solverId.solverId}' does not exist.`, { node: solverId, property: "solverId" }); 
+            }
+        }
+    }
+
+    async checkSolverSettingExists(solverSetting: SolverSetting, accept: ValidationAcceptor): Promise<void> {
+        await api.initialize(); // Ensure problem types are initialized before checking
+        const solverId = solverSetting.$container.solverId;
+        const problemTypeId = getProblemTypeBySolverId(solverId);
+        if (!problemTypeId) {
+            accept('error', `Solver ID '${solverId.solverId}' is not associated with any problem type.`, { node: solverId, property: "solverId" });
+            return;
+        }
+
+        if (solverId.solverId) {
+            const settings = await api.fetchSolverSettings(problemTypeId.id, solverId.solverId);
+            if (settings.filter(setting => setting.name === solverSetting.settingName).length === 0) {
+                accept('error', `Solver setting '${solverSetting.settingName}' does not exist for solver '${solverId.solverId}'.`, { node: solverSetting, property: "settingName" });
             }
         }
     }
