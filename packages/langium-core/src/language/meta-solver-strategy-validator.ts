30f850e98bef95fb9b63fe71e8f631cd117f79f8
Elscrux <nickposer2102@gmail.com>
2026-03-05 22:15:02 +0100

    fix: relative path

--- packages/langium-core/src/language/meta-solver-strategy-validator.ts
+++ packages/langium-core/src/language/meta-solver-strategy-validator.ts
@@ -1,5 +1,5 @@
 import { type ValidationAcceptor, type ValidationChecks } from 'langium';
-import type { MetaSolverStrategyServices } from 'langium-core/src/language/meta-solver-strategy-module.js';
+import type { MetaSolverStrategyServices } from './meta-solver-strategy-module.js';
 import { toolboxApi } from "./meta-solver-strategy-module.js";
 import { MetaSolverStrategyAstType, ProblemType, ProblemTypes, SolverID, ProblemAttribute, BoolExpression, Solver, SolverSetting, SolveProblem } from './generated/ast.js';
 import { getProblemTypeBySolverId, getProblemTypeByProblemName, getApplicableTypes, getType } from './utils/ast-utils.js';
